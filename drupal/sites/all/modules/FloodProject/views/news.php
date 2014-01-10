9c9753c3d8cec91a4f7b054bc9e26a50adaabebf
marcos henrich <marcoshenrich@gmail.com>
2014-01-10 12:32:05 +0000

    News add button is now only displayed when user has create privileges.

--- drupal/sites/all/modules/FloodProject/views/news.php
+++ drupal/sites/all/modules/FloodProject/views/news.php
@@ -1,12 +1,21 @@
 <?php
 
-$gid = false;
-if(active_module())
-    $gid = active_module()->gid;
+$add_link = url('node/add/blog');
+$user = $GLOBALS['user'];
 
-if(true){
+$module = active_module();
+if($module){
+    $gid = $module->gid;
+    $user_can_add = og_user_access_entity('create blog content', 'node', $module, $user);
+    $add_link .= '?modid=' . $module->nid;
+} else {
+    $gid = false;
+    $user_can_add = user_access('create blog content', $user);
+}
+
+if($user_can_add){
 ?>
-    <a href="" class="btn btn-xs btn-success">
+    <a href="<?php print $add_link ?>" class="btn btn-xs btn-success">
         <i class="icon glyphicon glyphicon-plus" aria-hidden="true"></i>
         Add News
     </a>
