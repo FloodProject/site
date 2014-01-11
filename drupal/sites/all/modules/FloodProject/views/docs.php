179e02224b1792b469353e222d5e852cdc525974
marcos henrich <marcoshenrich@gmail.com>
2014-01-11 17:19:41 +0000

    Implemented simple docs loading from file system and url requests.

--- drupal/sites/all/modules/FloodProject/views/docs.php
+++ drupal/sites/all/modules/FloodProject/views/docs.php
@@ -1,3 +1,17 @@
 <?php
 
-print " ModStir docs";
+$docs_dir = "C:\\docs";
+
+if(isset($_REQUEST['d']))
+{
+    $doc_file = realpath ($docs_dir . DIRECTORY_SEPARATOR . $_REQUEST['d']);
+    if(starts_with($doc_file, $docs_dir))
+    {
+        include $doc_file;
+        goto end;
+    }
+}
+
+print "No docs found.";
+
+end:
