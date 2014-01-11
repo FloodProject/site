34ecd8ba384ec779dd4dc16f506919ec10703eb6
marcos henrich <marcoshenrich@gmail.com>
2014-01-11 21:37:25 +0000

    Added helper function bootstrap_render_message;

--- drupal/sites/all/themes/bootstrap/theme/system/status-messages.func.php
+++ drupal/sites/all/themes/bootstrap/theme/system/status-messages.func.php
@@ -18,27 +18,39 @@ function bootstrap_status_messages($variables) {
     'info' => t('Informative message'),
   );
 
-  // Map Drupal message types to their corresponding Bootstrap classes.
-  // @see http://twitter.github.com/bootstrap/components.html#alerts
-  $status_class = array(
-    'status' => 'success',
-    'error' => 'danger',
-    'warning' => 'warning',
-    // Not supported, but in theory a module could send any type of message.
-    // @see drupal_set_message()
-    // @see theme_status_messages()
-    'info' => 'info',
-  );
-
   foreach (drupal_get_messages($display) as $type => $messages) {
-    $class = (isset($status_class[$type])) ? ' alert-' . $status_class[$type] : '';
+    $output .= bootstrap_render_message($type, $messages);
+  }
+  return $output;
+}
+
+function bootstrap_render_message($type, $messages){
+
+	if(is_string($messages))
+		$messages = array($messages);
+		
+	// Map Drupal message types to their corresponding Bootstrap classes.
+	// @see http://twitter.github.com/bootstrap/components.html#alerts
+	static $status_class = array(
+		'status' => 'success',
+		'error' => 'danger',
+		'warning' => 'warning',
+		// Not supported, but in theory a module could send any type of message.
+		// @see drupal_set_message()
+		// @see theme_status_messages()
+		'info' => 'info',
+	);
+	
+	$output = '';
+
+	$class = (isset($status_class[$type])) ? ' alert-' . $status_class[$type] : '';
     $output .= "<div class=\"alert alert-block$class\">\n";
     $output .= "  <a class=\"close\" data-dismiss=\"alert\" href=\"#\">&times;</a>\n";
 
     if (!empty($status_heading[$type])) {
       $output .= '<h4 class="element-invisible">' . $status_heading[$type] . "</h4>\n";
     }
-
+		
     if (count($messages) > 1) {
       $output .= " <ul>\n";
       foreach ($messages as $message) {
@@ -51,6 +63,7 @@ function bootstrap_status_messages($variables) {
     }
 
     $output .= "</div>\n";
-  }
-  return $output;
+	
+	return $output;
 }
+
